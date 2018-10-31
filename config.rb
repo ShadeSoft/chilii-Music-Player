0cf3618b19f6710973da1ea99ceea516e5820156
Szabolcs Surányi <szabolcs.suranyi@corpuscom.hu>
2018-10-31 15:40:11 +0100

    Move to Dart Sass from Compass

--- config.rb
+++
@@ -1,7 +0,0 @@
-require 'compass/import-once/activate'
-
-#http_path = "/"
-css_dir = "public/assets/css"
-sass_dir = "assets/scss"
-#images_dir = "img"
-#javascripts_dir = "js"
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 let gulp        = require('gulp');
 let babel       = require('gulp-babel');
-let compass     = require('gulp-compass');
 let concat      = require('gulp-concat');
 let del         = require('del');
 let minifyCss   = require('gulp-csso');
 let postcss     = require('gulp-postcss');
 let pump        = require('pump');
+let sass        = require('gulp-dart-sass');
 let uglifyJs    = require('gulp-uglify');
 
 gulp.task('jsClean', function(cb) {
@@ -29,11 +29,7 @@ gulp.task('jsWatch', function() {
 gulp.task('css', function(cb) {
     pump([
         gulp.src('assets/scss/**/*.scss'),
-        compass({
-            config_file:    './config.rb',
-            css:            'public/assets/css',
-            sass:           'assets/scss'
-        }),
+        sass().on('error', sass.logError),
         postcss([require('autoprefixer')]),
         minifyCss(),
         gulp.dest('public/assets/css')
