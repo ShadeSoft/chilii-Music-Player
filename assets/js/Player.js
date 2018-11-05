eaf374a3eaf9bdd814c8b47bab441cc0019a2ecf
Szabolcs Surányi <szabolcs.suranyi@corpuscom.hu>
2018-11-05 15:08:26 +0100

    Working on player

---
+++ assets/js/Player.js
@@ -0,0 +1,119 @@
+class Player {
+    constructor(audio, player) {
+        this.audio = $(audio);
+        this.player = $(player);
+        this.status = 'stopped';
+        this.data = {};
+
+        let instance = this;
+
+        this.audio.on('timeupdate', function() {
+            let currentTime = instance.audio[0].currentTime;
+            let duration = instance.audio[0].duration;
+            if(currentTime && duration) {
+                if(currentTime < duration) {
+                    instance.player.find('.progress-bar')
+                        .css('width', currentTime / duration * 100 + '%');
+                } else {
+                    instance.next();
+                }
+            }
+        });
+
+        this.player.find('.toggle-play').click(function() {
+            if(instance.status !== 'playing') {
+                instance.play();
+            } else {
+                instance.pause();
+            }
+        });
+
+        this.player.find('.play-prev').click(function() {
+            instance.restart();
+        });
+
+        this.player.find('.play-next').click(function() {
+            instance.next();
+        });
+    }
+
+    play(refresh = false) {
+        this.audio.trigger('play');
+        if(refresh) {
+            this.player.find('.title').html(this.data.title);
+            this.player.find('.artist').html(this.data.artist);
+            this.player.find('.album').html(this.data.album);
+            this.originalTitle = document.title;
+            document.title = '\u25B6 ' + this.data.title + ' - ' + this.data.artist + ' | chilii Music Player';
+        }
+        this.player.find('.fa-play')
+            .removeClass('fa-play')
+            .addClass('fa-pause');
+        this.status = 'playing';
+        return this;
+    }
+
+    pause() {
+        this.audio.trigger('pause');
+        this.player.find('.fa-pause')
+            .removeClass('fa-pause')
+            .addClass('fa-play');
+        this.status = 'paused';
+        return this;
+    }
+
+    restart() {
+        this.audio[0].currentTime = 0;
+        return this;
+    }
+
+    stop() {
+        this.pause().restart();
+        document.title = this.originalTitle;
+        return this;
+    }
+
+    next() {
+        if(this.nextHandler) {
+            this.nextHandler();
+        }
+    }
+
+    title(title = null) {
+        if(title) {
+            this.data.title = title;
+        }
+        return this.data.title;
+    }
+
+    artist(artist = null) {
+        if(artist) {
+            this.data.artist = artist;
+        }
+        return this.data.artist;
+    }
+
+    album(album = null) {
+        if(album) {
+            this.data.album = album;
+        }
+        return this.data.album;
+    }
+
+    setType(type) {
+        this.type = type;
+        this.audio.attr('type', this.type);
+        return this;
+    }
+
+    setSrc(src) {
+        this.src = src;
+        this.audio.attr('src', src);
+        return this;
+    }
+
+    setNextHandler(handler) {
+        this.nextHandler = handler;
+        return this;
+    }
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,40 @@
 $(document).ready(function() {
-    //var originalTitle = document.title;
-
     $(window).resize(function() {
-        $('main').css('margin-top', $('header').height());
+        $('main').css({
+            'margin-top': $('header').height(),
+            'margin-bottom': $('footer').height()
+        });
     }).resize();
 
-    $('#song').on('play', function() {
-        var icon = '\u25B6';
-        let audio = $(this);
-        let player = $('#player');
-
-        //originalTitle = document.title;
-        //document.title = icon + ' ' + $(this).data('title') + ' - ' + $(this).data('artist') + ' | chilii Music Player';
-
-        player.find('.title').html(audio.attr('data-title'));
-        player.find('.artist').html(audio.attr('data-artist'));
-        player.find('.album').html(audio.attr('data-album'));
-        player.find('.fa-play').removeClass('fa-play').addClass('fa-pause');
-    });
-
-    $('#song').on('pause', function() {
-        //document.title = originalTitle;
-        $('#player').find('.fa-pause').removeClass('fa-pause').addClass('fa-play');
-    });
-
-    $('#song').on('timeupdate', function() {
-        var audio = $(this)[0];
-        $('#player').find('.progress-bar').css('width', audio.currentTime / audio.duration * 100 + '%')
-    });
+    let song = new Player('#song', '#player');
 
     $(document).on('click', '.play-song', function() {
-        var sender = $(this);
-        var icon = sender.find('i');
-        $('#song').trigger('pause');
+        songHandler($(this));
+    });
+
+    let songHandler = function(sender) {
+        let icon = sender.find('i');
+        song.pause();
         if(icon.hasClass('fa-play-circle')) {
             $('.play-song').find('i').each(function() {
                 if($(this) !== sender) {
                     $(this).removeClass('fa-pause-circle').addClass('fa-play-circle');
                 }
             });
-            $('#song')
-                .attr('type', $(this).data('type'))
-                .attr('src', $(this).data('src'))
-                .attr('data-title', $(this).data('title'))
-                .attr('data-artist', $(this).data('artist'))
-                .attr('data-album', $(this).data('album'));
-            $('#song').trigger('play');
+            song
+                .setType(sender.data('type'))
+                .setSrc(sender.data('src'));
+
+            song.title(sender.data('title'));
+            song.artist(sender.data('artist'));
+            song.album(sender.data('album'));
+            song.play(true);
+
+            let next = parseInt(sender.attr('rel')) + 1;
+            song.setNextHandler(function() {
+                songHandler($('#play-song-' + next));
+            });
         }
         icon.toggleClass('fa-play-circle').toggleClass('fa-pause-circle');
-    });
+    }
 });
